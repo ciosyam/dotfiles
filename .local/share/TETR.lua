eadfb8bc36eaf6ce8cc2164d37890c27b50a9c22
tetr-a <tetr-a@tetr.com>
2024-08-13 06:51:29 +0700

    13agustus

--- .local/share/TETR.lua
+++ .local/share/TETR.lua
@@ -1,81 +1,66 @@
-
--- Credits to original theme for existing https://github.com/shaunsingh/oxocarbon.nvim
--- this is a modified version of it
-
 local M = {}
 
 M.base_30 = {
-  white = "#F9f5d7",
-  darker_black = "#000000",
-  black = "#000000", -- nvim bg
-  black2 = "#0A0A0A",
-  one_bg = "#121212", -- slightly lighter background
-  one_bg2 = "#1A1A1A",
-  one_bg3 = "#232323",
-  grey = "#2B2B2B",
-  grey_fg = "#333333",
-  grey_fg2 = "#3B3B3B",
-  light_grey = "#434343",
-  red = "#F50093",
-  baby_pink = "#F50093",
-  pink = "#F50093",
-  line = "#2A2A2A", -- for lines like vertsplit
-  green = "#0d0efe", -- blue for green
-  vibrant_green = "#0d0efe",
-  nord_blue = "#0d0efe",
-  blue = "#5AFF15", -- green for blue
-  yellow = "#F50093",
-  sun = "#F50093",
-  purple = "#F50093",
-  dark_purple = "#F50093",
-  teal = "#F50093",
-  orange = "#F50093",
-  cyan = "#F50093",
-  statusline_bg = "#000000",
-  lightbg = "#121212",
-  pmenu_bg = "#F50093", -- magenta for menu
-  folder_bg = "#F50093", -- magenta for folders
-  lavender = "#F50093",
+  white = "#ffffff",
+  darker_black = "#000000",  -- rich black
+  black2 = "#010203",        -- chinese black
+  one_bg = "#080808",        -- eerie black
+  one_bg2 = "#2b1329",        -- slight black shade
+  one_bg3 = "#501a4f",        -- deeper black shade
+  grey = "#808080",          -- grayscale
+  grey_fg = "#a31ea2",       -- desaturated fuchsia
+  grey_fg2 = "#781e77",      -- darker desaturated fuchsia
+  light_grey = "#d018d0",    -- fuchsia shade
+  red = "#ff00ff",           -- fuchsia
+  baby_pink = "#ff5aff",     -- lighter fuchsia
+  pink = "#ff83ff",          -- lighter fuchsia
+  line = "#2b1329",          -- subtle line color
+  green = "#00ff00",         -- complement
+  vibrant_green = "#00c4ff", -- tetradic cyan
+  nord_blue = "#0036ff",     -- analogue blue
+  blue = "#ff009c",          -- analogue pink
+  yellow = "#ff24ff",        -- monochromatic shade
+  sun = "#ff37ff",           -- monochromatic shade
+  purple = "#de00e0",        -- monochromatic shade
+  dark_purple = "#e900ea",   -- monochromatic shade
+  teal = "#ff47ff",          -- monochromatic shade
+  orange = "#ff5aff",        -- lighter fuchsia
+  cyan = "#ff00ff",          -- fuchsia
+  statusline_bg = "#010203", -- chinese black
+  lightbg = "#080808",       -- eerie black
+  pmenu_bg = "#00ff00",      -- complement
+  folder_bg = "#ff00ff",     -- analogue blue
+  lavender = "#ff5aff",      -- lighter fuchsia
 }
 
 M.base_16 = {
-  base00 = "#000000",
-  base01 = "#121212",
-  base02 = "#1A1A1A",
-  base03 = "#232323",
-  base04 = "#F9f5d7",
-  base05 = "#F9f5d7",
-  base06 = "#F9f5d7",
-  base07 = "#F50093",
-  base08 = "#F50093",
-  base09 = "#5AFF15",
-  base0A = "#F50093",
-  base0B = "#5AFF15",
-  base0C = "#F50093",
-  base0D = "#0d0efe", -- blue variation
-  base0E = "#F50093",
-  base0F = "#F50093",
+  base00 = "#000000",        -- vampire black
+  base01 = "#141414",        -- chinese black
+  base02 = "#1b1b1b",        -- eerie black
+  base03 = "#2b1329",        -- slight black shade
+  base04 = "#501a4f",        -- deeper black shade
+  base05 = "#fcfafa",        -- white
+  base06 = "#ff00ff",        -- fuchsia
+  base07 = "#ff5aff",        -- lighter fuchsia
+  base08 = "#ff00ff",        -- fuchsia
+  base09 = "#0036ff",        -- analogue blue
+  base0A = "#ff009c",        -- analogue pink
+  base0B = "#00ff00",        -- complement
+  base0C = "#ff83ff",        -- lighter fuchsia
+  base0D = "#ff24ff",        -- monochromatic shade
+  base0E = "#ff47ff",        -- monochromatic shade
+  base0F = "#e900ea",        -- monochromatic shade
 }
 
 M.polish_hl = {
   syntax = {
-    Conditional = { fg = "#F50093" },
-    Function = { fg = "#5AFF15" },
-    Identifier = { fg = "#5AFF15" },
-    Keyword = { fg = "#5AFF15" },
-    Number = { fg = "#5AFF15" },
-    Operator = { fg = "#F50093" },
-    Statement = { fg = "#5AFF15" },
-    String = { fg = "#F50093" },
-    Tag = { fg = "#F9f5d7" },
-    Type = { fg = "#5AFF15" },
-    Comment = { fg = "#0d0efe" }, -- using the blue variation for comments
+    Conditional = { fg = M.base_30.nord_blue },
+    Tag = { fg = M.base_30.white },
   },
 }
 
 M.type = "dark"
 
-M = require("base46").override_theme(M, "oxocarbon")
+M = require("base46").override_theme(M, "TETR")
 
 return M
-
